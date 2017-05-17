866f645e0397359f03411d3e457c928e76863f31
ChoiSeJong <glowglowglow@hanmail.net>
2017-05-17 09:55:35 +0900

    textarea tabkey modify

--- javascript/script.js
+++ javascript/script.js
@@ -80,6 +80,19 @@ var control = document.getElementById("inputfile");
          reader.readAsText(control.files[0]);
      }, false);
 
+var textareas = document.getElementsByTagName('textarea');
+var count = textareas.length;
+for(var i=0;i<count;i++){
+   textareas[i].onkeydown = function(e){
+       if(e.keyCode==9 || e.which==9){
+           e.preventDefault();
+           var s = this.selectionStart;
+           this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+           this.selectionEnd = s+1;
+       }
+   }
+}
+
 var divs = ["introduce","example","main","contact"];
 for(var i=0 ; i<4; i++){
   var divId = document.getElementById(divs[i]);
